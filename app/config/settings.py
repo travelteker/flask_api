d31e40c97a39e6a6584cb7bc2612e7d63690fe41
devjpozo <devjpozo@gmail.com>
2021-05-10 16:13:32 +0200

    -- fix -- Refactoring configuration file to personalize CONNECTION URI for database MONGO. Adding directory venv/ to .gitignore file

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,7 +5,8 @@ ENV = getenv('ENV')
 DEBUG = bool(int(getenv('DEBUG')))
 
 
-MONGO_URI = f'mongodb+srv://{getenv("DB_USER")}:{getenv("DB_PASSWORD")}@cluster0.oeact.mongodb.net/' \
-            f'{getenv("DB_NAME")}?retryWrites=true&w=majority'
+# MONGO_URI = f'mongodb+srv://{getenv("DB_USER")}:{getenv("DB_PASSWORD")}@cluster0.oeact.mongodb.net/' \
+#             f'{getenv("DB_NAME")}?retryWrites=true&w=majority'
 
-
+MONGO_URI = f'{getenv("DB_SCHEME")}://{getenv("DB_USER")}:{getenv("DB_PASSWORD")}@{getenv("DB_PATH")}/' \
+            f'{getenv("DB_NAME")}?{getenv("DB_QUERY")}'
